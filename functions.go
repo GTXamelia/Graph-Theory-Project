da2176551019b0856b7e4963d0934fe535ee1871
Cian Gannon <lego@live.ie>
2018-03-09 21:41:42 +0000

    Foundation code setup

---
+++ functions.go
@@ -0,0 +1,12 @@
+package graphProject
+
+// Imports used
+import (
+
+)
+
+// Processes user input to become more readable by eliza
+func inputSep(input string) string {
+    
+    return "test";
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+// Imports used
+import (
+	".."
+)
+
+// Main function handles HTTP requests
+func main() {
+	
+	testInput := "Testing123"
+
+	test := inputSep(testInput);
+}
