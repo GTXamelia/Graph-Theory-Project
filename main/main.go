5a7f623a786b6fafd7785a7ccb8956879c1fdab2
Cian Gannon <lego@live.ie>
2018-04-02 21:30:23 +0100

    Added another go file to hold postfix and clear up the main.go file

---
+++ main/main.go
@@ -0,0 +1,44 @@
+package main
+
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	".."
+)
+
+
+// This function removed last two values from a string
+// Getting user input adds two ascii codes that mess up post fix
+// The 13 and 10 are for next line 13 = Carriage return, 10 = Line feed
+// These two number would still be evaluated by postfix even if they are invisible to the user
+// Example:
+// input = a.b.c*
+// ascii before = [97 46 98 46 99 42 '13 10']
+// ascii after  = [97 98 46 99 '13 10' 42 46]
+// output = ab.c
+// *.
+func TrimSuffix(s string) string {
+    if len(s) > 0 {
+		s = s[:len(s)-2]
+	}
+    return s
+}
+
+type state struct {
+	symbol rune
+	edge1 *state
+	edge2 *state
+}
+
+func main() {
+	
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter query: ")
+	input, _ := reader.ReadString('\n')
+	input = TrimSuffix(input)
+	input = postfix.Intopost(input) // Remove ending of string
+	fmt.Println("Result", input)
+
+}
---
+++ postfix.go
@@ -0,0 +1,35 @@
+package postfix
+
+import (
+)
+
+func Intopost(infix string) string {
+	specials := map[rune]int{'*':10, '.':9, '|':8}
+
+	pofix, s:= []rune{}, []rune{}
+
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			s = append(s, r)
+		case r == ')':
+			for s[len(s)-1] != '('{
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = append(s, r)
+		default:
+			pofix = append(pofix, r)
+		}
+	}
+
+	for len(s) > 0 {
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}
+
+	return string(pofix)
+}
