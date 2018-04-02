3ddc0786edc87112fb6fb917cfe16895edaad455
Cian Gannon <lego@live.ie>
2018-04-02 19:39:15 +0100

    Added switch statement for postfix function

--- main.go
+++ main.go
@@ -10,7 +10,24 @@ func intopost(infix string) string {
 
 	pofix, s:= []rune{}, []rune{}
 
-
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			s = append(s, r)
+		case r == ')':
+			for s[len(s)-1] != '('{
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = append(s, r)
+		default:
+			pofix = append(pofix, r)
+		}
+	}
 
 
 	return string(pofix)
