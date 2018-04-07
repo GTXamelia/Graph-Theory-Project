3c5c8baa8d873d5baea27976b3500609716ac311
Cian Gannon <lego@live.ie>
2018-04-07 23:42:01 +0100

    More comments to ConcatAuto

--- support.go
+++ support.go
@@ -18,54 +18,58 @@ func ConcatAuto(infix string) string {
 	// Loops through string character by character
 	for infixChar := 0; infixChar < len(infix); infixChar++ {
 
+		// If character == 0
 		if infixChar == 0 {
-			
-			buffer.WriteString(string(Arr[infixChar]))
-			continue
+			buffer.WriteString(string(Arr[infixChar])) // add character to buffer
+			continue // Continue to next loop iteration
 		}
+		// If character == "." skip
 		if string(Arr[infixChar]) == "." {	
-			continue
+			continue // Continue to next loop iteration
 		}
+		// If character is one of the 'specials'
 		if specialsCheck(string(Arr[infixChar])) {
 
+			// If character == "|" then check is true this is used later to concatanate or not
 			if !(string(Arr[infixChar]) == "|") {
 				check = false
 			}else{
 				check = true
 			}
 
-			buffer.WriteString(string(Arr[infixChar]))
-			continue
+			buffer.WriteString(string(Arr[infixChar])) // add character to buffer
+			continue // Continue to next loop iteration
 		}
+		// character == "(" and is not the first character of the string
 		if !(infixChar == 0) && string(Arr[infixChar]) == "(" {
-
-			buffer.WriteString(".")
-			buffer.WriteString(string(Arr[infixChar]))
-			continue
+			buffer.WriteString(".") // add to buffer
+			buffer.WriteString(string(Arr[infixChar])) // Add character to buffer// Add character to buffer
+			continue // Continue to next loop iteration
 		}
+		// Checks the previous character == ")"
 		if !(infixChar == 0) && string(Arr[infixChar-1]) == "(" {
-
-			buffer.WriteString(string(Arr[infixChar]))
-			continue
+			buffer.WriteString(string(Arr[infixChar])) // add character to buffer
+			continue // Continue to next loop iteration
 		}
+		// If character == ")"
 		if string(Arr[infixChar]) == ")" {
-
-			buffer.WriteString(string(Arr[infixChar]))
-			continue
+			buffer.WriteString(string(Arr[infixChar])) // add character to buffer
+			continue // Continue to next loop iteration
 		}
+		// If character is a letter (Using ASCII values between/and 65 and 122)
 		if (Arr[infixChar] >= 65 && Arr[infixChar] <= 122) {
+			// If check is true then previous character was "|"
 			if check {
-				buffer.WriteString(string(Arr[infixChar]))
+				buffer.WriteString(string(Arr[infixChar])) // Add character to buffer
 			}else {
-				buffer.WriteString(".")
-				buffer.WriteString(string(Arr[infixChar]))
+				buffer.WriteString(".") // add to buffer
+				buffer.WriteString(string(Arr[infixChar])) // Add character to buffer
 			}
-			continue
+			continue // Continue to next loop iteration
 		}
 	}
 
-	fmt.Println(buffer.String())
-	return buffer.String()
+	return buffer.String() // Return string
 }
 
 func specialsCheck(char string) bool {
